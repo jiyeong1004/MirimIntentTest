5209b886b70a2110cfeef5a7dd2ebe95c8b8771a
jiyeong1004 <ak1157@naver.com>
2020-11-26 20:06:33 +0900

    Complete

--- app/src/main/java/com/example/mirimintenttest/MainActivity.java
+++ app/src/main/java/com/example/mirimintenttest/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.mirimintenttest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText input1, input2;
+    RadioButton btn_plus, rdo_minus, rdo_multi, rdo_divide;
+    Button btn_result;
+    RadioGroup rg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnResult = findViewById(R.id.btn_result);
+        rg = findViewById(R.id.rg);
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+
+        btnResult.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtra("input1", input1.getText().toString());
+            intent.putExtra("input2", input2.getText().toString());
+
+            String op = "";
+
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.rdo_plus:
+                    op = "더하기";
+                    break;
+                case R.id.rdo_minus:
+                    op = "빼기";
+                    break;
+                case R.id.rdo_multi:
+                    op = "곱하기";
+                    break;
+                case R.id.rdo_divide:
+                    op = "나누기";
+                    break;
+            }
+
+            intent.putExtra("op", op);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/mirimintenttest/ResultActivity.java
+++ app/src/main/java/com/example/mirimintenttest/ResultActivity.java
@@ -1,6 +1,10 @@
 package com.example.mirimintenttest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,39 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        TextView textResult = findViewById(R.id.text_result);
+
+        Intent intent = getIntent();
+        int input1 = Integer.parseInt(intent.getStringExtra("input1"));
+        int input2 = Integer.parseInt(intent.getStringExtra("input2"));
+
+        String op = intent.getStringExtra("op");
+
+        String result = "";
+
+        switch (op){
+            case "더하기":
+                result = input1 + " + " + input2 + " = " + (input1 + input2);
+                break;
+            case "빼기":
+                result = input1 + " - " + input2 + " = " + (input1 - input2);
+                break;
+            case "곱하기":
+                result = input1 + " * " + input2 + " = " + (input1 * input2);
+                break;
+            case "나누기":
+                result = input1 + " / " + input2 + " = " + (input1 / input2);
+                break;
+        }
+
+        textResult.setText(result);
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
